591024550a22031ed7d87984c480aa617ce78672
Marco-Passa <passa.marco@outlook.it>
2024-03-04 17:30:56 -0800

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
     
     socket.on('transcription', (data) => {
       console.log('Audio transcripted');
-      setTranscription(oldTranscription => oldTranscription + ' ' + data.transcription);
+      setTranscription(data.transcription);
     });
 
     return () => {
@@ -29,53 +29,24 @@ function App() {
     socket.connect();
   };
 
-  const start = async () => {
-    try {
-      // Infinite loop that stops and restarts recording every 3 seconds
-      while (true) {
-        console.log('ciao1')
-        console.log('ciao2')
-        const recording = await startRecording();
-        console.log('ciao3')
-        await wait(); // Wait for 3 seconds
-        console.log('ciao4')
-        stopRecording(recording); // Stop recording
-        console.log('ciao5')
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   const startRecording = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const mediaRecorderInstance = new MediaRecorder(stream);
       mediaRecorderInstance.ondataavailable = handleDataAvailable;
+      mediaRecorderInstance.start();
       setAudioStream(stream);
       setMediaRecorder(mediaRecorderInstance);
-
-      // Start recording initially
-      mediaRecorderInstance.start();
-
-      return { stream, mediaRecorderInstance };
     } catch (error) {
       console.error('Error accessing microphone:', error);
     }
   };
 
-  const stopRecording = (recording) => {
-    if (recording && recording.mediaRecorderInstance) {
-      recording.mediaRecorderInstance.stop();
-      recording.stream.getTracks().forEach(track => track.stop());
+  const stopRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+      audioStream.getTracks().forEach(track => track.stop());
     }
-  };
-
-  // Define a function to wait for 3 seconds
-  const wait = async () => {
-    return new Promise(resolve => {
-      setTimeout(resolve, 5000);
-    });
   };
 
   const handleDataAvailable = (event) => {
@@ -87,12 +58,14 @@ function App() {
     reader.readAsDataURL(event.data);
   };
 
+
+
   return (
     <div>
       <button onClick={connectBackend}>Connect to Backend</button>
       {connected && (
         <div>
-          <button onClick={start}>Start Recording</button>
+          <button onClick={startRecording}>Start Recording</button>
           <button onClick={stopRecording}>Stop Recording</button>
           <p>Transcription: {transcription}</p>
         </div>
